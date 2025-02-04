4a37c31616d6d77819c47671e590a034fbd27a8e
adity72dd <mmunnabot7@gmail.com>
2025-02-04 17:30:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 BOT_TOKEN = "7843184227:AAFyvygYudOXjSkfbXTioBljy9IC-NJXfOQ"
 
 # Enter your Telegram username (without @)
-OWNER_USERNAME = "Riyahacksyt"
+OWNER_USERNAME = "NARUTO_OWNERS"
 
 # Enter your admin ID (Get it from @MissRose_bot by typing /info)
 ADMIN_IDS = ["6930050494"]  # Keep it as a list for multiple admins
