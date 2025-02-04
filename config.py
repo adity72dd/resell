d13d57dfd54d7c3cf8fc372d0f2ce423f509b8d2
adity72dd <mmunnabot7@gmail.com>
2025-02-04 18:21:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ ADMIN_IDS = ["6479495033"]  # Keep it as a list for multiple admins
 CHANNEL_LINK = "https://t.me/+EiUj5ibb1V1kZDM1"  # Replace with your actual channel link
 
 # Enter the filename of your channel logo (Image must be in the same folder as the script)
-CHANNEL_LOGO = "channel_logo.png"  # Replace with actual image file name
+CHANNEL_LOGO = "channel_logo.jpg"  # Replace with actual image file name
