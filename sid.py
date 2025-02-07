1b869c87e960b036c7d3003525e4aede99ab8003
adity72dd <mmunnabot7@gmail.com>
2025-02-07 17:45:00 +0530

    Update sid.py

--- sid.py
+++ sid.py
@@ -9,8 +9,8 @@ import time
 
 USER_FILE = "users.json"
 ADMIN_FILE = "admins.json"
-DEFAULT_THREADS = 3000
-DEFAULT_PACKET = 20
+DEFAULT_THREADS = 1700
+DEFAULT_PACKET = 9
 DEFAULT_DURATION = 240  # Default attack duration
 
 # Store the last attack time for each user (in seconds)
